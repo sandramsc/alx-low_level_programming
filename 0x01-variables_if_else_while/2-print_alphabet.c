8129613dd5c7c04cd870a71b19835283871bb5a8
Sandra Ashipala <sandraashipala@gmail.com>
2023-04-22 18:36:25 -0700

    fixed betty style error #1

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,12 +5,14 @@
 
 /* betty style doc for function main goes there */
 /**
- * main - I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game
+ * main - I sometimes suffer from insomnia. And when
+ * I can't fall asleep, I play what I call the alphabet game
  * Return: Always 0
  */
 int main(void)
 {
 	char c;
+
 	for (c = 'a'; c <= 'z'; ++c)
 	{
 		putchar(c);
