105ac6f35033b9f46bfc2360a8dc8d1f16854c7c
Sandra Ashipala <sandraashipala@gmail.com>
2023-04-23 14:24:00 -0700

    adjusted function for output with $ sign

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Patience, persistence and perspiration make an 
+ * unbeatable combination for success
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+	
+	for (i = 0; i < 10; i++)
+	{
+			putchar(i + '0');
+			/*add separator if not the last number*/
+			if (i < 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	/*print the final character*/
+	putchar('\n');
+			
+return (0);
+}
