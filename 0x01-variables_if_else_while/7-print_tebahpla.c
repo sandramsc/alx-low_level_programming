c82f4324be8fc59ac8c2098d70b510b8bb92f729
Sandra Ashipala <sandraashipala@gmail.com>
2023-04-23 13:10:28 -0700

    first commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Smile in the mirror
+ * Return: Always 0
+ */
+int main(void)
+{
+	char c;
+	
+	/* print alphabet backwards  */
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+return (0);
+}
