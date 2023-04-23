c667f73794bcf26153fcc36fc5b4e94dad05cbc3
Sandra Ashipala <sandraashipala@gmail.com>
2023-04-22 18:55:19 -0700

    first commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - alphABET
+ * Return: Always 0
+ */
+int main(void)
+{
+	/* Printing the lowercase alphabet */
+	char c = 'a';
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	/* Printing the uppercase alphabet */
+	c  = 'A';
+	while (c <= 'Z')
+	{
+		putchar(c);
+		c++;
+	}
+return(0);
+}
