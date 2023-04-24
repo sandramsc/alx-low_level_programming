284b9a5abf1eb5f0e8f2f11d862ae4fd1406762f
Sandra Ashipala <sandraashipala@gmail.com>
2023-04-24 16:31:11 -0700

    added 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/**
+ * main - Hello, puts
+ * 
+ * Return : Always 0 (Success)
+ */
+int main (void)
+{
+	char *str = "\"Programming is like building a multilingual puzzle";
+	puts(str);
+
+return (0);
+}
