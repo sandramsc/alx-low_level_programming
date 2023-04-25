c5f03d834576f5c199d4e6ebca71b8f72f0f628d
Sandra Ashipala <sandraashipala@gmail.com>
2023-04-25 15:22:13 -0700

    added 1-alphabet.c file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+/**
+ * main - I sometimes suffer from insomnia. And when I can't fall asleep,
+ * I play what I call the alphabet game
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c<='z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+return (0);
+}
