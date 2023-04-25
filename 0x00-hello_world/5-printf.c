ea09dc366312635d25e233d9b57adfd2b0e1b735
Sandra Ashipala <sandraashipala@gmail.com>
2023-04-24 17:03:11 -0700

    added 5-printf.c file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Hello, printf
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+return (0);
+}
