aac0998aeb16916340f4f2115ed8d3bd6f8d4b57
Sandra Ashipala <sandraashipala@gmail.com>
2023-04-24 19:31:36 -0700

    betty coding style fix #2

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,12 +3,12 @@
  * main - UNIX is basically a simple operating system, but you have
  * to be a genius to understand the simplicity
  *
- * Return: Always 1
+ * Return: 1 (Success)
  */
 int main(void)
 {
 	const char msg[] = "and that piece of art is useful\" "
-		"- Dora Korpar, 2015-10-19\n";
+			"- Dora Korpar, 2015-10-19\n";
 
 	write(2, msg, sizeof(msg) - 1);
 	return (1);
