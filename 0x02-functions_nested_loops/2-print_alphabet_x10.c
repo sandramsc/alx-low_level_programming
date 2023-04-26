63a39170c116e3f72503b0654c14c1fe759b15d0
Sandra Ashipala <sandraashipala@gmail.com>
2023-04-26 14:30:04 -0700

    added 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+/**
+ * main - 10 x alphabet
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char c;
+	int i;
+
+	for (i  = 0; i < 10; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			putchar(c);
+		}
+	putchar('\n');
+	}
+return (0);
+}
