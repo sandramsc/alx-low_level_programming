56acc45a02a4a950dc1f42ff229e073b4e450fcf
Sandra Ashipala <sandraashipala@gmail.com>
2023-04-27 08:37:53 -0700

    4-isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isalpha  - isalpha
+ * @c: The charcter that needs to be checked
+ * Return: 1 for lowercase characteror 0 for anything else
+ */
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 98 ) || (c >= 97 && c <= 122))
+	{
+	return (1);
+	}
+	return(0);
+}
