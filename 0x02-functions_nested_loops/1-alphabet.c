ea4a00fd0c66caed546e39018dff3eacfa78b225
Sandra Ashipala <sandraashipala@gmail.com>
2023-04-27 06:57:23 -0700

    fix betty coding style #1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * print_alphabet  - I sometimes suffer from insomnia. And when I can't fall asleep,
- * I play what I call the alphabet game
+ * print_alphabet  - I sometimes suffer from insomnia.
+ * And when I can't fall asleep, I play what I call the alphabet game
  *
  * Return: void
  */
