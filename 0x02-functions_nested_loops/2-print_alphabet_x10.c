b7c9809b83b34eec868055099cda197f5e61f7dd
Sandra Ashipala <sandraashipala@gmail.com>
2023-04-27 08:09:48 -0700

    fix betty coding style erro

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,11 +11,11 @@ void print_alphabet_x10(void)
 
 	while (i <= 9)
 	{
-		for (c = 'a'; c <= 'z'; c++)
+	for (c = 'a'; c <= 'z'; c++)
 	{
 	_putchar(c);
 	}
 	_putchar('\n');
-        i++;
+	i++;
         }
 }
