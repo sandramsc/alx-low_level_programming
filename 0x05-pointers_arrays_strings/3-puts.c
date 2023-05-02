a950903894c9a017b1efba4d8cb4dc614ce9d7df
Sandra Ashipala <sandraashipala@gmail.com>
2023-05-02 10:10:10 -0700

    fix betty coding style

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
- * _puts - A program  that prints a string, followed by a new line, to stdout 
+ * _puts - A program  that prints a string, followed by a new line, to stdout
+ * @str: the string 
  * Return: empty
  */
 void _puts(char *str)
 {
 	/*Loop until the end of the string is reached*/
-	while(*str != '\0')
+	while (*str != '\0')
 	{
 		/*Print the current character*/
 		_putchar(*str++);
