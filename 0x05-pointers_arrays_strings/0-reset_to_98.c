75008d09fc3264444283105978ee6597eb46e27b
Sandra Ashipala <sandraashipala@gmail.com>
2023-05-02 07:10:40 -0700

    added description for parameter n

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,6 +3,7 @@
 /**
  * reset_to_98 - A program that takes a pointer to an int as parameter
  * and updates the value it points to
+ * @n: The number that needs to be evaluated
  * Return: empty
  */
 void reset_to_98(int *n)
