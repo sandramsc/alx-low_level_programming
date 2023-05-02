78d5255c2dc9bb76a52bb60412c255097f19c0a7
Sandra Ashipala <sandraashipala@gmail.com>
2023-05-02 09:13:01 -0700

    added 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen - A program that returns the length of a string
+ * @s: string variable
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+	len++;
+	s++;
+	}
+
+	return len;
+}
