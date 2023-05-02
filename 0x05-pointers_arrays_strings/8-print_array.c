a8cdb08056168787abd30dedbb81de9e4dccaa12
Sandra Ashipala <sandraashipala@gmail.com>
2023-05-02 13:13:38 -0700

    added descriptions for *a and n

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -3,6 +3,8 @@
 #include <string.h>
 /**
  * print_array - A program that prints n elements of an array of integers
+ * @a: array
+ * @n: integers
  * Return: empty
  */
 void print_array(int *a, int n)
