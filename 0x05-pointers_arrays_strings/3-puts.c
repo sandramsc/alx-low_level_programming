76731e84e84a56336dc870f5076fa53b3a512e6d
Sandra Ashipala <sandraashipala@gmail.com>
2023-05-02 10:05:45 -0700

    added 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts - A program  that prints a string, followed by a new line, to stdout 
+ * Return: empty
+ */
+void _puts(char *str)
+{
+	/*Loop until the end of the string is reached*/
+	while(*str != '\0')
+	{
+		/*Print the current character*/
+		_putchar(*str++);
+	}
+	_putchar('\n');
+}
