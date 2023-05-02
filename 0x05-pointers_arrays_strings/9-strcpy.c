853c172918bab75c75f176a525c0793123928ee3
Sandra Ashipala <sandraashipala@gmail.com>
2023-05-02 12:37:10 -0700

    fixed description for _strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * *_strcpy - A program that copies the string pointed to by src, including
+ * _strcpy - A program that copies the string pointed to by src, including
  * the terminating null byte (\0), to the buffer pointed to by dest
  * @dest: Pointer to the destination buffer.
  * @src: Pointer to the source string
