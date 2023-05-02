698aa09ad2e41e464fea8822b2dc23153381c4eb
Sandra Ashipala <sandraashipala@gmail.com>
2023-05-02 10:34:18 -0700

    fix betty coding style #1

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,18 +1,19 @@
 #include "main.h"
 #include <string.h>
 /**
- * print_rev - A program that prints a string, in reverse, followed by a new line
+ * print_rev - A program that prints a string, in reverse,
+ * followed by a new line
  * @s: string characheter
  * Return: empty
  */
 void print_rev(char *s)
 {
 	int len = strlen(s);
-	int i;
+	int n;
 
-	for (i = len - 1; i >= 0; i--)
+	for (n = len - 1; n >= 0; n--)
 	{
-	_putchar(s[i]);
+	_putchar(s[n]);
 	}
 	_putchar('\n');
 }
