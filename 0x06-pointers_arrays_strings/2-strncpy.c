b0a8a00597c369b0fbd81cddb214ba314f805dc2
Sandra Ashipala <sandraashipala@gmail.com>
2023-05-04 13:13:37 -0700

    fix betty coding style issues #1

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -5,12 +5,14 @@
  * _strncpy - A program that copies a string
  * @dest: Pointer to the destination buffer.
  * @src: Pointer to the source string
+ * @n: The number of bytes from src to be appended to dest
  * Return: A pointer to the resulting string dest
  */
 
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
+
 	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 	dest[i] = src[i];
@@ -19,5 +21,5 @@ char *_strncpy(char *dest, char *src, int n)
 	{
 	dest[i] = '\0';
 	}
-	return dest;
+	return (dest);
 }
