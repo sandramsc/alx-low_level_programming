cac0a67cf651b6888a2c0bba7c4f4cf7fce18e9e
Sandra Ashipala <sandraashipala@gmail.com>
2023-05-08 15:26:40 -0700

    added 2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+*  _strchr- A program that locates a character in a string
+* @s: input string
+* @c: character to be located
+* @n: number of bytes to copy
+* Return: Always 0 (Success)
+*/
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+	/* If the current character matches the target..*/
+	/*..character, return a pointer to it */
+	if (*s == c)
+	{
+	return (s);
+	}
+	/* increment the pointer to the next character in the string */
+	s++;
+
+}
+return (NULL);
+}
