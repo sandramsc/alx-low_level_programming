705bb9a585aa904b505c431d69973a47c3f94a1a
Sandra Ashipala <sandraashipala@gmail.com>
2023-05-08 14:47:57 -0700

    added 1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+*  _memcpy- A program that
+* @src: Pointer to the source string
+* @dest: Pointer to the destination buffer
+* @n: number of bytes to copy
+* Return: A pointer to dest buffer
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *p = dest;
+
+	/* Use a pointer tothe dest buffer and copy
+	each byte of the src buffer into it */
+	while (n--)
+	{
+	*p++ = *src++;
+	}
+
+return (dest);
+}
