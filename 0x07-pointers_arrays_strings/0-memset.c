5b9cc7bb69a54338752963e6a0c3a7ace1986c68
Sandra Ashipala <sandraashipala@gmail.com>
2023-05-08 14:25:29 -0700

    fix betty coding style issue #1

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,17 +2,21 @@
 /**
 *  _memset- A program that
 * @n: number of bytes
-*
+* @s: memory area
 * Return: pointer to the memory area s
 */
 
 char *_memset(char *s, char b, unsigned int n)
 {
+	/* Initialize pointer to memory area s*/
 	char *p = s;
 
+	/* Loop through n bytes in the memory area pointed to by s */
 	while (n--)
 	{
+	/* Set the byte pointed to by p to b, and increment p to the next byte */
 	*p++ = b;
 }
+/* Reurn a pointer to the beginning of the memory area s */
 return (s);
 }
