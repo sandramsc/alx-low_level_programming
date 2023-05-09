cffed4bedfc3663497b8507262c9ea2a14501ba4
Sandra Ashipala <sandraashipala@gmail.com>
2023-05-09 05:53:27 -0700

    fix code errors

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,29 +1,26 @@
 #include "main.h"
 
 /**
-* print_diagsums - A program that prints the sum of the two
-* diagonals of a square matrix of integers
-* @a: pointer to the first element of the matrix
+* print_chessboard - A program that prints the sum of the two diagonals of a square matrix of integers
 * @sum1: first diagonal
 * @sum2: second diagonal
-* @size: matrix size
-*
-* Return: Always 0 (Success)
+* Return: nothing
 */
 
 void print_diagsums(int *a, int size)
 {
-	/* initialize two sums to zero */
-	int sum1 = 0, sum2 = 0, i;
-
+	/* initialize two sums to zero and i*/
+	int i, sum1 = 0, sum2 = 0;
+	
 	/* iterate over the rows of the matrix */
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size * size; i += size + 1)
 	{
 	/* add element on diagonal from top left to bottom right to sum1 */
-	sum1 += *(a + i * size + i);
-	/*add element on diagonal from top right to bottom left to sum2 */
-	sum2 += *(a + i * size + (size - i - 1));
+	sum1 += a[i];
 	}
-	/* Print the sums using printf() */
+	for (i = size - 1; i < size * size - 1; i += size - 1)
+	{
+	sum2 += a[i];
+	}
 	printf("%d, %d\n", sum1, sum2);
 }
