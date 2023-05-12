5a4790b1b4b4982820faf857c4c8d8b32550d7da
Sandra Ashipala <sandraashipala@gmail.com>
2023-05-12 14:22:12 -0700

    changed function to remove segmentaion error

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -5,18 +5,35 @@
 /**
 * is_palindrome -  a program that returns 1
 * if a string is a palindrome and 0 if not
-* @s: the string to check
+* @start: the string to check
+* @end: integer
 * Return: 1 if the string is a palindrome, 0 otherwise
 */
 
+int is_palindrome_helper(char *s, int start, int end)
+{
+	if (start >= end)
+	{
+	return (1);
+	}
+	if (s[start] == s[end])
+	{
+	return (is_palindrome_helper(s, start + 1, end - 1));
+	}
+	else
+	{
+	return (0);
+}
+}
+/**
+* is_palindrome -  a program that returns 1
+* if a string is a palindrome and 0 if not
+* @s: the string to check
+* Return: 1 if the string is a palindrome, 0 otherwise
+*/
 int is_palindrome(char *s)
 {
 	int len = strlen(s);
-
-	if (len <= 1)
-	{
-	/*empty string or single character is a palindrome*/
-	return (1);
-	}
-	return ((*s == *(s + len - 1))  && is_palindrome(s + 1));
+	
+	return (is_palindrome_helper(s, 0, len -1));
 }
