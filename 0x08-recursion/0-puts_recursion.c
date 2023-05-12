709fa789d9aa5d40de94fe28d9dfe8940e51c8f2
Sandra Ashipala <sandraashipala@gmail.com>
2023-05-12 02:33:36 -0700

    fix betty coding style issue #1

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,13 +9,15 @@
  */
 void _puts_recursion(char *s)
 {
-	/*Base case: If the first character is the null character, print a new line and return*/
+	/*Base case: If the first character is the..*/
+	/*..null character, print a new line and return..*/
 	if (*s == '\0')
 	{
 	putchar('\n');
 	return;
 	}
-	/*Recursive case: Print the first character, and then call the function recursively with the rest of the string*/
+	/*Recursive case: Print the first character, and then call the function..*/
+	 /*.. recursively with the rest of the string*/
 	putchar(*s);
 	_puts_recursion(s + 1);
 }
