dcbc4aafa54c7032288545826e3a4c20c078207e
Sandra Ashipala <sandraashipala@gmail.com>
2023-05-13 08:56:56 -0700

    added the <unistd.h> header to puts function

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,10 +1,12 @@
-#include "main.h"
-#include <stdio.h>
+#include <unistd.h>
 
 /**
- * _putchar - description
- */
+*_putchar - write the character c to stdout
+*@c:The character to print
+*Return 1 if true, 0 if false
+*On error, -1 return.
+*/
 int _putchar(char c)
 {
-        return (0);
+	return (write(1, &c, 1));
 }
