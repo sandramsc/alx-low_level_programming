29df6b81e07fd6cbc9474028bbaa6806c60ef13e
Sandra Ashipala <sandraashipala@gmail.com>
2023-05-17 09:08:28 -0700

    fix betty coding style errors

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
-/*
- * main - This program prints the name of
- * the executable file followed by a new line.
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints the name of the 
+ * executable file followed by a new line.
  * If the name of the file is changed, the program will print the new name
  * without having to recompile the program.
  *
  * @argc: The number of command line arguments and the size of the argv array
+ * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
