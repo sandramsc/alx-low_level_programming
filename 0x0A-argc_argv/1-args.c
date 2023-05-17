e80c3ddc2fa93b3ea39f0b1c5065ddba06b76898
Sandra Ashipala <sandraashipala@gmail.com>
2023-05-17 10:49:34 -0700

    fix betty coding style

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,12 +5,15 @@
  *
  * Description: This program prints the number of arguments passed to it.
  *
+ * Parameters:
  * @argv: An array of strings containing the command line arguments
  * @argc: The number of command line arguments and the size of the argv array
+ *
  * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
+	/*To suppress unused parameter warning*/
 	(void) argv;
 
 	/*Subtract 1 to exlude the program name*/
