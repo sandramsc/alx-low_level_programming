76523e571e91f2fe46590957c0d286cd76a9828f
Sandra Ashipala <sandraashipala@gmail.com>
2023-05-19 05:54:47 -0700

    fix for expected checker outcome #2

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -4,8 +4,13 @@
 #include <stdlib.h>
 /**
  * malloc_checked - Entry point of the program
+ *
  * Description: This program allocates memory using malloc.
+ *
+ * Parameters:
  * @ptr: pointer where memory is allocated
+ * @b: the size of the memory to be allocated in bytes
+ *
  * Return: A pointer to the allocated memory
  */
 void *malloc_checked(unsigned int b)
