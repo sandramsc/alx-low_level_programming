0ad407ddd58f2ff0b13b33bb757b790b8048bd4e
Sandra Ashipala <sandraashipala@gmail.com>
2023-05-22 16:59:23 -0700

    first commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,14 +1,33 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include <stdlib.h>
+
 /**
  * struct dog - a new type struct dog
- *
  * @name: Dog name
  * @age: Dog age
  * @owner: Dog owner
- * 
+ *
  */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
+};
+
+struct dog *new_dog(char *name, float age, char *owner)
+{
+	struct dog *my_dog;
+
+	my_dog = malloc(sizeof(struct dog));
+	if (my_dog == NULL)
+		return (NULL);
+	my_dog->name = name;
+	my_dog->age = age;
+	my_dog->owner = owner;
+	return (my_dog);
 }
+
+#endif
