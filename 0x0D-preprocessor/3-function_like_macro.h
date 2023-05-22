ce49a396de134db8304d08349fe1124f80386e71
Sandra Ashipala <sandraashipala@gmail.com>
2023-05-22 11:01:46 -0700

    first commit

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,8 +1,10 @@
 #ifndef ABS_MACRO
 #define ABS_MACRO
 
+/* function-like macro ABS(x) that computes the absolute value of a number x */
 #ifndef ABS
+/*check the location of the arithmetic operator to optain expected output*/
 #define ABS(x) (x < 0 ? -(x) : (x))
 #endif
-/* function-like macro ABS(x) that computes the absolute value of a number x */
+
 #endif
