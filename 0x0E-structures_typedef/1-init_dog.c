8d6891e015c17e7e096834bc88bcc19aae09c801
Sandra Ashipala <sandraashipala@gmail.com>
2023-05-23 16:31:01 -0700

    refactored task 0 connected to followed tasks

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,13 +1,13 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef INIT_HEADER
+#define INIT_HEADER
 
+#include "dog.h"
 #include <stdio.h>
-#include "main.h"
-
+#include <stdlib.h>
 /**
  * init_dog - a program that initializes a variable of type struct dog
  *
- * @d: pointer to members
+ * @d: the structure of the dog
  * @name: Dog name
  * @age: Dog age
  * @owner: Dog owner
@@ -17,9 +17,9 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
 	{
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
 	}
 }
 #endif
