c2c33a3642799221cb89644f56733ea2f05931fd
Sandra Ashipala <sandraashipala@gmail.com>
2023-05-24 11:43:50 -0700

    fix for expected output

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -11,12 +11,10 @@
  *
  * Parameters:
  * @name: the name
- * @f: the pointer function
+ * @f: the input function that prints the name
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (f != 0 && name != 0)
-	
 	f(name);
 }
 
