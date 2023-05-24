1c294d75653b6d10ba8de8cb93964451e62477a3
Sandra Ashipala <sandraashipala@gmail.com>
2023-05-24 03:55:00 -0700

    fix betty style errors

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -42,6 +42,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_dog->owner = malloc(sizeof(char) * (strlen(owner) + 1));
 	if (new_dog->owner == NULL)
 	{
+		free(new_dog->name);
 		free(new_dog);
 		return (NULL);
 	}
