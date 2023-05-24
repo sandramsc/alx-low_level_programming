ddc7a3e5d5139799e5171adbe79e79fcc73c3b31
Sandra Ashipala <sandraashipala@gmail.com>
2023-05-24 04:10:27 -0700

    fix for expected output

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -9,12 +9,17 @@
  * free_dog - a program that frees dogs
  *
  * @d: the structure of the dog
+ *
+ * Return: nothing
  */
 
 void free_dog(dog_t *d)
 {
+	if (d)
+	{
 	free(d->owner);
 	free(d->name);
 	free(d);
+	}
 }
 #endif
